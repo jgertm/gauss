6d13badeac9e03b02b23072e08020c7cbd923a1a
Tim Jäger <jger.tm@gmail.com>
2017-08-11 22:24:24 -0700

    Switch to hpack

---
+++ src/Prelude.hs
@@ -0,0 +1,13 @@
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+{-# OPTIONS_GHC -Wno-dodgy-exports #-}
+
+module Prelude
+  ( module Prelude
+  , module Data.Proxy
+  , module ClassyPrelude
+  , printf
+  ) where
+
+import           ClassyPrelude
+import           Data.Proxy
+import           Text.Printf   (printf)
