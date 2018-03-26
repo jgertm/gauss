b5494746b11f3ee236abba0e172431ff23710c73
Tim Jäger <jger.tm@gmail.com>
2018-03-25 17:54:20 -0700

    Actually do stuff!

--- src/Dev.hs
+++ src/Dev.hs
@@ -1,9 +1,11 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Dev where
 
-import           Control.Monad
-import           Data.Vector   (Vector)
+import           Universum   hiding (reduce)
+
+import           Data.Vector (Vector)
 
 
 type Scalar = Double
@@ -15,69 +17,109 @@ data Expression = Variable String
                 | Application Operation [Expression]
                 deriving (Show, Eq)
 
-getOp :: Expression -> Maybe Operation
-getOp (Application op _) = Just op
-getOp (Variable _)       = Nothing
-getOp (Constant _)       = Nothing
+op :: Expression -> Maybe Operation
+op (Application op _) = Just op
+op _                  = Nothing
+
+args :: Expression -> Maybe [Expression]
+args (Application _ args) = Just args
+args _                    = Nothing
+
 
 instance Num Expression where
   x + y = Application Addition [x,y]
   x * y = Application Multiplication [x,y]
-  x - y = Application Subtraction [x,y]
-  negate x = 0 - x
+  x - y = Application Addition [x, Application Negation [y]]
+  negate x = Application Negation [x]
   abs = undefined
   signum = undefined
   fromInteger = Constant . fromInteger
 
 instance Fractional Expression where
-  x / y = Application Division [x,y]
-  recip x = 1 / x
+  x / y = Application Multiplication [x, Application Inversion [y]]
+  recip x = Application Inversion [x]
   fromRational = Constant . fromRational
 
 data Operation = Addition
                | Multiplication
-               | Subtraction
-               | Division
+               | Negation
+               | Inversion
                | Exponentiation
                | Log
                deriving (Show, Eq)
 
 isGroup :: Operation -> Bool
-isGroup op = op `elem` [Addition, Multiplication, Exponentiation]
+isGroup op = op `elem` map operation groups
 
-getGroup :: Operation -> Maybe Group
-getGroup Addition = Just $ Group { operation = Addition, inverse = Subtraction, unit = 0 }
-getGroup Multiplication = Just $ Group { operation = Multiplication, inverse = Division, unit = 0 }
-getGroup _ = Nothing
+getGroupByOperation, getGroupByInverse :: Operation -> Maybe Group
+getGroupByOperation op = find (\g -> op == operation g) groups
+getGroupByInverse iop = find (\g -> iop == inverse g) groups
+
+groups :: [Group]
+groups =
+  [ Group { operation = Addition, inverse = Negation, unit = 0 }
+  , Group { operation = Multiplication, inverse = Inversion, unit = 1 }
+  ]
 
 data Group = Group { operation :: Operation
                    , inverse   :: Operation
-                   , unit      :: Scalar
+                   , unit      :: Expression
                    }
 
-foo :: Expression
-foo = 1 + 2
-
-baz :: Expression
+foo, baz, bar, quux :: Expression
+foo = 1 + 0
 baz = (-(-1))
-
-bar :: Expression
 bar = 1 + (-1)
-
-quux :: Expression
 quux = 2 * (1/2)
 
 type RewriteRule = Expression -> Maybe Expression
 
-removeLeftUnitApplication, removeDoubleInverse, reduceInverseApplication :: RewriteRule
+doNothing, removeLeftUnitApplication, removeRightUnitApplication, removeUnitApplication, removeDoubleInverse, removeInverseApplication :: RewriteRule
+
+doNothing = Just
 
 removeLeftUnitApplication (Application op [unit', val]) = do
-  Group{inverse, unit} <- getGroup op
+  Group{unit} <- getGroupByOperation op
   guard $ unit == unit'
-  undefined
+  pure val
+removeLeftUnitApplication _ = Nothing
 
-removeDoubleInverse (Application negO [unitO, Application negI [unitI, val]]) = undefined
--- removeDoubleInverse expr | maybe False isGroup . getOp $ expr = undefined
---                          | otherwise = Nothing
+removeRightUnitApplication (Application op [val, unit']) = do
+  Group{unit} <- getGroupByOperation op
+  guard $ unit == unit'
+  pure val
+removeRightUnitApplication _ = Nothing
 
-reduceInverseApplication expr = undefined
+removeUnitApplication = liftA2 (<|>) removeLeftUnitApplication removeRightUnitApplication
+
+removeDoubleInverse (Application negO [Application negI [val]]) = do
+  guard $ negO == negI
+  Group{} <- getGroupByInverse negO
+  pure val
+removeDoubleInverse _ = Nothing
+
+removeInverseApplication (Application op [x,y]) = do
+  Group{unit, inverse} <- getGroupByOperation op
+  let xinv = Application inverse [x]
+  xinv' <- removeDoubleInverse xinv <|> pure xinv
+  guard $ xinv' == y
+  pure unit
+removeInverseApplication _ = Nothing
+
+rules :: [RewriteRule]
+rules = [ doNothing
+        , removeLeftUnitApplication
+        , removeRightUnitApplication
+        , removeDoubleInverse
+        , removeInverseApplication
+        ]
+
+-- quux = Application Multiplication [Constant 2.0,Application Multiplication [Constant 1.0,Application Inversion [Constant 2.0]]]
+
+-- reduce :: Expression -> Expression
+reduce (Application op args) = catMaybes $ do
+  args' <- sequence $ map (\a -> catMaybes $ map ($ a) rules) args
+  let expr = Application op args'
+  rule <- rules
+  pure $ rule expr
+reduce expr = [expr]
